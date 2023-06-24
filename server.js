0ee64ca3447d30ad7278813bf3d7d5f605e51006
Suraksha8-Sadhwani <suraksha.bscsf19@iba-suk.edu.pk>
2023-06-24 15:36:34 +0500

    delete

--- server.js
+++ server.js
@@ -57,6 +57,25 @@ app.get("/api/blogs/:id", async (req, res) => {
     }
   });
 
+//Delete Blog
+app.delete("/api/blogs/:id", async (req, res) => {
+    const { id } = req.params;
+  
+    var response = await BlogModel.findById(id);
+  
+    if (!response) {
+      return res.status(401).send({ message: "No such blog found" });
+    }
+  
+    response = await BlogModel.deleteOne({ _id: id });
+    res.status(200).send({ message: "Deleted Blog", data: response });
+  
+    try {
+    } catch (error) {
+      res.status(500).send({ message: error });
+    }
+  });
+
 app.listen(8000, () => {
   console.log("Server running on port 8000");
 });
