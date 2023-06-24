3505ad5f83f27151e641b1ff0baaeb23545fb076
Suraksha8-Sadhwani <suraksha.bscsf19@iba-suk.edu.pk>
2023-06-24 15:30:54 +0500

    create

---
+++ server.js
@@ -0,0 +1,26 @@
+require("./database/conn");
+
+const express = require("express");
+const BlogModel = require("./model/blog.model");
+const app = express();
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+
+
+//Create
+app.post("/api/blogs", async (req, res) => {
+  const response = await BlogModel.create(req.body);
+  res.status(200).send({ message: "Blog Created", data: response });
+
+  try {
+  } catch (error) {
+    res.status(500).send({ message: error });
+  }
+});
+
+
+
+app.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
